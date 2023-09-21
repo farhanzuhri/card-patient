ef9dc58c64f5529086d42a8014fedc54cf9c5340
farhanzuhri <101404632+farhanzuhri@users.noreply.github.com>
2023-09-21 16:40:06 +0700

    Update login.php

--- Front-End/login/login.php
+++ Front-End/login/login.php
@@ -16,7 +16,7 @@
     <div class="global-container">
       <div class="card login-form">
         <div class="card-body">
-          <h1 class="card-title text-center">M A S U K</h1>
+          <h1 class="card-title text-center">L O G I N</h1>
         </div>
         <div class="card-text">
           <form>

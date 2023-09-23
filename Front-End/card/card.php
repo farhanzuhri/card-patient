2ea1691a4d9d1d662b26a20b97f47feb6ba2559e
farhanzuhri <101404632+farhanzuhri@users.noreply.github.com>
2023-09-23 20:29:50 +0700

    add card

---
+++ Front-End/card/card.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Register</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="global-container">
+      <div class="card card-form">
+        <div class="card-body">
+          <h2 class="card-title">Kartu Pasien</h2>
+        </div>
+        <hr />
+        <div class="card-text">
+          <form>
+            <div class="row mb-3">
+              <label for="inputEmail3" class="col-sm-2 col-form-label"
+                >ID Pasien</label
+              >
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="inputEmail3" />
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label for="inputEmail3" class="col-sm-2 col-form-label"
+                >Nama</label
+              >
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="inputEmail3" />
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label for="inputEmail3" class="col-sm-2 col-form-label"
+                >Umur</label
+              >
+              <div class="col-sm-10">
+                <input type="date" class="form-control" id="inputEmail3" />
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label for="inputEmail3" class="col-sm-2 col-form-label"
+                >Tanggal Lahir</label
+              >
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="inputEmail3" />
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label for="inputEmail3" class="col-sm-2 col-form-label"
+                >Alamat</label
+              >
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="inputEmail3" />
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label for="inputEmail3" class="col-sm-2 col-form-label"
+                >RT / RW</label
+              >
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="inputEmail3" />
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label for="inputEmail3" class="col-sm-2 col-form-label"
+                >Kelurahan / Desa</label
+              >
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="inputEmail3" />
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label for="inputEmail3" class="col-sm-2 col-form-label"
+                >No Telepon</label
+              >
+              <div class="col-sm-10">
+                <input type="number" class="form-control" id="inputEmail3" />
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label for="inputEmail3" class="col-sm-2 col-form-label"
+                >Gender</label
+              >
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="inputEmail3" />
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
